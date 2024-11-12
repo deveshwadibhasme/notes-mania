fc22968881331316e72ac7cad569079e1bb9b149
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-11-12 18:40:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,8 @@ let menuToggle = document.querySelector('.menu');
 
 menuToggle.addEventListener('click', ()=>{
     nav.classList.toggle('open-nav');
+     setTimeout(() => {
+    nav.classList.remove('open-nav');
+    },3000)
 })
 
