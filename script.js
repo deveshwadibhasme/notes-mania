37db115192438d07763742366b6912f0e4de946f
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-11-12 17:43:05 +0530

    mohit-idea

---
+++ script.js
@@ -0,0 +1,7 @@
+let nav = document.querySelector('nav');
+let menuToggle = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', ()=>{
+    nav.classList.toggle('open-nav');
+})
+
