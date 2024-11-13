42b985619ed6ed0e48a7bbdb20846480f934a279
deveshwadibhasme <146552994+deveshwadibhasme@users.noreply.github.com>
2024-11-13 09:44:06 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,8 +3,21 @@ let menuToggle = document.querySelector('.menu');
 
 menuToggle.addEventListener('click', ()=>{
     nav.classList.toggle('open-nav');
-     setTimeout(() => {
+    setTimeout(() => {
     nav.classList.remove('open-nav');
     },3000)
 })
+
+function streamSelection(){
+    let selectValueClass = document.querySelector('.select-class') 
+    let selectStream = document.querySelector('.select-stream')
+    selectValueClass.addEventListener('change',(e)=>{
+        if(parseInt(e.target.value) <= 10){
+            selectStream.setAttribute('disabled','true')
+        }else{
+            selectStream.removeAttribute('disabled')
+        }
+    })
+}
+streamSelection();
 
